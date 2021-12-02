853c48f7e10fb49479c9b839078b0987ad51f685
Dickson200055272 <200055272@stu.vtc.edu.hk>
2021-12-03 00:11:23 +0800

    boomboomboom

--- RTS_TVEAB2/Assets/NuEATest01/Scripts/Chess/Player/Player_Agent.cs
+++ RTS_TVEAB2/Assets/NuEATest01/Scripts/Chess/Player/Player_Agent.cs
@@ -10,8 +10,8 @@ public class Player_Agent : MonoBehaviour
 
     private NavMeshAgent navMeshAgent;
     SelectableCharacter selected;
-    private Rigidbody rb;
-    Vector3 destination;
+    //private Rigidbody rb;
+    public Vector3 destination;
     [Range(1.0f,100f)]public float moveSpeed;
     void Awake()
     {
@@ -24,10 +24,11 @@ void Update()
     {
      if (selected == true && Input.GetMouseButtonDown(1))
         {
+            
             if (Vector3.Distance(destination, movePosTransform.position) > 1.0f)
             {
-                destination = movePosTransform.position;
-                navMeshAgent.destination = destination;
+            destination = movePosTransform.position;
+            navMeshAgent.destination = destination;
             }
         }
     }
